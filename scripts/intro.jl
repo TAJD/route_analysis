08c0c6806b5b59fa6229497805ab70ee325fd400
td7g11@soton.ac.uk <tom0dickson@gmail.com>
2019-05-15 14:05:58 +0100

    setting up project

--- scripts/intro.jl
+++ scripts/intro.jl
@@ -1,3 +1,7 @@
 using DrWatson
+quickactivate(@__DIR__, "routing_analysis")
+using Revise, sail_route
 
-DrWatson.greet()
+
+println(sail_route.typical_aerrtsen())
+println(sail_route.generate_performance(120.0, [0.0, 5.0, 10.0, 20.0, 21.0], 0.3))
