decebe3a127ba5a8bbeffa84943719400f63e993
td7g11@soton.ac.uk <tom0dickson@gmail.com>
2019-06-18 06:26:30 +0100

    change input settings

--- src/load_performance.jl
+++ src/load_performance.jl
@@ -2,3 +2,4 @@ using DrWatson, PyCall
 perf = pyimport("routing_helper")
 
 println(perf.generate_performance(120.0, [0.0, 5.0], 0.3))
+println(perf.generate_circular_performance(120.0, [0.0, 5.0, 10.0, 20.0, 21.0], 0.3))
--- src/load_route_settings.jl
+++ src/load_route_settings.jl
@@ -1,10 +1,10 @@
 
 function generate_route_settings()
-    start_loc_names = ["Vavau", "Tongatapu", "Eiao", "Mangaia"]
-    finish_loc_names = ["Upolu", "Tahiti", "Tupuai", "Tauranga"]
-    start_lat = [-18.65, -21.21, -8, -21.92]
-    start_lon = [-173.98, -175.15, -140.7, -157.92]
-    finish_lat = [-13.91, -17.67, -23.38, -37.68]
-    finish_lon = [-171.75, -149.42, -149.45, 176.17]
+    start_loc_names = ["Vavau", "Tongatapu"]
+    finish_loc_names = ["Upolu", "Tahiti"]
+    start_lat = [-18.65, -21.21]
+    start_lon = [-173.98, -175.15]
+    finish_lat = [-13.91, -17.67]
+    finish_lon = [-171.75, -149.42]
     return start_loc_names, finish_loc_names, start_lat, start_lon, finish_lat, finish_lon
 end
